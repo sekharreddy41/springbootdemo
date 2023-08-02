256e218c97f1a35ee0cb7831e708c752404429e6
sekhar <VsekharreddyY@gmail.com>
2023-08-02 15:42:29 +0530

    Commit

---
+++ src/main/java/com/dailyCodeBuffer/springBootDemoIM/SpringBootDemoImApplication.java
@@ -0,0 +1,13 @@
+package com.dailyCodeBuffer.springBootDemoIM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoImApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoImApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailyCodeBuffer/springBootDemoIM/SpringBootDemoImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyCodeBuffer.springBootDemoIM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoImApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
